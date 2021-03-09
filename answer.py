70c06ceecc451c39f712caf403aba029c56a898c
Ben Lao <benlau413@gmail.com>
2021-03-09 23:18:53 +0800

    finish Q3 and add questions

--- answer.py
+++ answer.py
@@ -7,13 +7,12 @@ if __name__ == "__main__":
         Usage: pi [partitions]
     """
 
-    from pyspark import SparkContext
-    sc = SparkContext("local", "First App")
+    spark = SparkSession\
+        .builder\
+        .appName("Assignment1")\
+        .getOrCreate()
 
-    # spark = SparkSession\
-    #     .builder\
-    #     .appName("Assignment1")\
-    #     .getOrCreate()
+    sc = spark.sparkContext
 
     def logParse(log):
         log = log.replace(' -- ', ', ')
@@ -29,24 +28,46 @@ if __name__ == "__main__":
         parsedRDD = textFile.map(logParse).filter(lambda x: x is not None)
         return parsedRDD
 
-    rowrdd = loadRDD("torrent-logs2.txt").cache()
+    rowrdd = loadRDD("torrent-logs.txt").cache()
 
     def getINFONumber(rowrdd):
         INFONumber = rowrdd.filter(lambda x: x[0] == "INFO").count()
         return INFONumber
 
     def getRepoName(x):
-        DividedURL = x[4].split('/')
-        RepoName = DividedURL[4] + "/" + DividedURL[5].split("?")[0]
+        dividedURL = x[4].split('/')[4:6]
+        if len(dividedURL) == 0:
+            return None
+        elif len(dividedURL) == 1:
+            dividedURL.append('')
+        RepoName = (dividedURL[0] + "/" + dividedURL[1]).split("?")[0]
         return RepoName
 
     def getProcessedRepositoriesNumber(rowrdd):
-        RepoUrlList = rowrdd.filter(lambda x: x[3] == "api_client")
-        response = RepoUrlList.map(lambda x: getRepoName(x)).groupBy(lambda x: x)
-        return response.count()
-    
-    print("1. Ans :%s\n" % getINFONumber(rowrdd))
+        repoUrlList = rowrdd.filter(lambda x: x[3] == "api_client")
+        repoTotal = repoUrlList.map(lambda x: getRepoName(x)).filter(lambda x: x is not None)
+        uniqueRepoGroup = repoTotal.groupBy(lambda x: x)
+        return uniqueRepoGroup.count()
 
-    print("2. Ans :%s\n" % getProcessedRepositoriesNumber(rowrdd))
+    def getFailedIDRequest(x):
+        if x[4].split(" ")[0] == "Failed":
+            return x[2]
+        else:
+            return None
 
-    # print(rowrdd.collect())
+    def getIDOfMostFailed(rowrdd):
+        requestList = rowrdd.filter(lambda x: x[3] == "api_client")
+        failedRequestList = requestList.map(lambda x: getFailedIDRequest(x)).filter(lambda x: x is not None).map(lambda x: (x, 1))
+        reducedFailedRequestList = failedRequestList.reduceByKey(lambda x, y: x + y)
+        mostFailedIDInfo = reducedFailedRequestList.max(key = lambda x: x[1])
+        return mostFailedIDInfo
+
+    print("Q1. Count the number of messages in the category of “INFO”.")
+    # print("Q1 Ans: The number of “INFO” messages are %d.\n" % getINFONumber(rowrdd))
+
+    print("Q2. Based on the information of retrieval stage “api_client”, count the number of processed repositories.")
+    # print("Q2 Ans: The number of processed repositories are %d.\n" % getProcessedRepositoriesNumber(rowrdd))
+
+    print("Q3. Which client (downloader id) did most FAILED HTTP requests?")
+    mostFailedID, mostFailedIDCount = getIDOfMostFailed(rowrdd)
+    print("Q3. Ans: The client id is '%s', it did %d times FAILED HTTP requests.\n" % (mostFailedID, mostFailedIDCount))
